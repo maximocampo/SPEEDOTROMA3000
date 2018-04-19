26551d7589d693eb018a86cad12a251b8082a515
Maximo Campo <themaxomoto@gmail.com>
2018-04-19 14:12:35 -0500

    juego terminado, faltan detalles

---
+++ js/classes.js
@@ -0,0 +1,322 @@
+//constructores
+function Background(){
+  this.x = 0;
+  this.y = 0;
+  this.width = canvas.width;
+  this.height = canvas.height;
+  this.imgMountainsOrange = new Image()
+  this.imgMountainsOrange.src = './assets/montañasNaranja.png'
+  this.imgMountainsPurple = new Image()
+  this.imgMountainsPurple.src = './assets/montañasRosa.png'
+  this.imgMountainsOrange2 = new Image()
+  this.imgMountainsOrange2.src = './assets/montañasNaranja2.png'
+  this.imgMountainsPurple2 = new Image()
+  this.imgMountainsPurple2.src = './assets/montañasRosa2.png'
+
+  this.move = function(){
+    if(this.x < -this.width * 2) this.x = 0;
+    this.x-= 7;
+  }
+  this.draw = function(){
+    this.move()
+    this.drawSky(color);
+    this.drawMountain2(this.x)
+    this.drawMountain1(this.x)
+
+  }
+
+  this.drawSky = function(x){
+      ctx.fillStyle = x
+      ctx.fillRect(this.x,this.y,this.width * 3,this.height)
+  }
+
+  this.drawMountain1 = function(){
+    ctx.drawImage(this.imgMountainsOrange,this.x * 2,this.y + 70,this.width,this.height)
+    ctx.drawImage(this.imgMountainsOrange2,this.x*2 +this.width,this.y + 70,this.width,this.height)
+    ctx.drawImage(this.imgMountainsOrange,this.x*2 + this.width * 2,this.y + 70,this.width,this.height)
+    ctx.drawImage(this.imgMountainsOrange2,this.x*2 + this.width * 3,this.y + 70,this.width,this.height)
+    ctx.drawImage(this.imgMountainsOrange,this.x*2 + this.width * 4,this.y + 70,this.width,this.height)
+  }
+  this.drawMountain2 = function(){
+    ctx.drawImage(this.imgMountainsPurple,this.x,this.y,this.width,this.height)
+    ctx.drawImage(this.imgMountainsPurple2,this.x + this.width,this.y,this.width,this.height)
+    ctx.drawImage(this.imgMountainsPurple,this.x + this.width * 2,this.y,this.width,this.height)
+  }
+}
+function CrazyHead(){
+  this.x = 340
+  this.y = 100
+  this.width = 320;
+  this.height = 160;
+  this.frame = 0
+  this.img = new Image()
+  this.img.src = './assets/crazyhead.png'
+
+  this.draw = function(){
+    ctx.drawImage(this.img,this.frame * 320,0,this.width,this.height, this.x, this.y,this.width,this.height)
+    if(mainmenuframes % 32 === 0)this.frame++
+    if(this.frame > 1) this.frame = 0
+  }
+
+}
+function Block(x,y){
+  this.x = x ? x : canvas.width
+  this.y = y;
+  this.width = 32;
+  this.height = 32;
+  this.img = new Image()
+  this.img.src = './assets/block.png'
+
+  this.draw = function(){
+    ctx.drawImage(this.img,this.x,this.y,this.width,this.height)
+    this.x-=2
+  }
+}
+function SpikeBlock(x,y){
+  this.x = x ? x : canvas.width
+  this.y = y;
+  this.width = 32;
+  this.height = 40;
+  this.img = new Image()
+  this.img.src = './assets/spikeblock.png'
+
+  this.draw = function(){
+    ctx.drawImage(this.img,this.x,this.y - 8,this.width,this.height)
+    this.x-=2
+  }
+}
+function LadderBlock(x,y){
+  this.x = x ? x : canvas.width
+  this.y = y;
+  this.width = 32;
+  this.height = 32;
+  this.img = new Image()
+  this.img.src = './assets/stairblock.png'
+
+  this.draw = function(){
+    ctx.drawImage(this.img,this.x,this.y,this.width,this.height)
+    this.x -= 2
+  }
+}
+function StickyBlock(x,y){
+  this.x = x ? x : canvas.width
+  this.y = y;
+  this.width = 32;
+  this.height = 36;
+  this.img = new Image()
+  this.img.src = './assets/stickyblock.png'
+
+  this.draw = function(){
+    ctx.drawImage(this.img,this.x,this.y,this.width,this.height)
+    this.x -= 2
+  }
+}
+function Structure(){
+  this.x                  = canvas.width
+  this.y                  = canvas.height - 64
+  this.structureSelector  = [
+                              [ new Block(this.x + 160,this.y),
+                                new Block(this.x,this.y -32),
+                                new Block(this.x+32,this.y -32),                               
+                                new Block(this.x + 160,this.y -32),
+                                new Block(this.x,this.y -64),
+                                new Block(this.x + 128,this.y -64),
+                                new Block(this.x + 160,this.y -64),
+                                new Block(this.x,this.y -96),
+                                new Block(this.x,this.y -128),
+                                new Block(this.x,this.y -160),
+                                new Block(this.x + 32,this.y -160),
+                                new StickyBlock(this.x + 64,this.y -160),
+                                new StickyBlock(this.x + 96,this.y -160),
+                                new StickyBlock(this.x + 128,this.y -160),
+                                new Block(this.x + 160,this.y -160),
+                              ],
+                              [ new Block(this.x,this.y),
+                                new Block(this.x + 32,this.y),
+                                new Block(this.x + 64,this.y),
+                                new Block(this.x + 224,this.y),
+                                new Block(this.x + 256,this.y),
+                                new Block(this.x + 32,this.y - 32),
+                                new Block(this.x + 64,this.y - 32),
+                                new Block(this.x + 128,this.y - 32),
+                                new Block(this.x + 160,this.y - 32),
+                                new Block(this.x + 256,this.y - 32),
+                                new Block(this.x + 64,this.y - 64),
+                                new Block(this.x + 128,this.y - 64),
+                                new Block(this.x + 128,this.y - 96),
+                                new Block(this.x + 160,this.y - 96),
+                                new Block(this.x + 192,this.y - 96),
+                                new Block(this.x + 224,this.y - 96),
+                                new Block(this.x + 256,this.y - 96)
+                              ],
+                              [ new Block(this.x,this.y),
+                                new Block(this.x + 128,this.y),
+                                new Block(this.x + 160,this.y),
+                                new Block(this.x + 192,this.y),
+                                new Block(this.x + 64,this.y - 32),
+                                new Block(this.x + 160,this.y - 32),
+                                new Block(this.x + 192,this.y - 32),
+                                new LadderBlock(this.x + 32,this.y - 64),
+                                new Block(this.x + 64,this.y - 64),
+                                new Block(this.x + 96,this.y - 64),
+                                new Block(this.x + 192,this.y - 64),
+                                new LadderBlock(this.x + 32,this.y - 96),
+                                new Block(this.x + 192,this.y - 96),
+                                new Block(this.x + 96,this.y - 128),
+                                new Block(this.x + 128,this.y - 128),
+                                new Block(this.x + 160,this.y - 128),
+                                new Block(this.x + 192,this.y - 128),
+                              ],
+                              [ new Block(this.x,this.y),
+                                new SpikeBlock(this.x + 32,this.y + 31.5),
+                                new Block(this.x + 64,this.y),
+                                new SpikeBlock(this.x + 96,this.y + 31.5),
+                                new Block(this.x + 128,this.y)
+                              ],
+                              [ new Block(this.x,this.y),
+                                new SpikeBlock(this.x + 32,this.y + 31.5),
+                                new Block(this.x + 64,this.y),
+                                new Block(this.x + 128,this.y),
+                                new SpikeBlock(this.x + 96,this.y + 31.5),
+                                new Block(this.x + 64,this.y-32),
+                                new Block(this.x + 128,this.y-32),
+                              ],
+                              [ 
+                                new Block(this.x + 64,this.y-32),
+                                new Block(this.x + 128,this.y-32),
+                              ]
+                            ]
+  this.img = new Image()
+  this.img.src = './assets/block.png'
+
+  this.draw = function(){
+    structures.push(this.structureSelector[structureNumber])
+  this.x -= 2
+}
+}
+function Player(image){
+  this.x         = canvas.width + 40;
+  this.y         = 500;
+  this.width     = 20;
+  this.height    = 29;
+
+  this.jumping   = false;
+  this.jumpStrength = 2
+  this.velY = 0;
+  this.velX = 0;
+  this.speed = 3;
+  this.grounded = false
+  this.keys = []
+
+  this.frame     = 2;
+  this.sheet     = new Image()
+  this.sheet.src = image
+ 
+
+  this.runAnimation = function () {
+    this.x-=2
+    ctx.drawImage(this.sheet,this.frame * 21 ,0,this.width,this.height + 2, this.x, this.y,this.width,this.height)
+
+    this.y += this.velY;
+    this.velY += gravity;
+    
+    this.x += this.velX;
+    this.velX *= friction;
+  }
+  this.move = function(){
+    if(this.keys[38]){
+      if(!this.jumping){
+        this.velY = -this.jumpStrength * 2;
+        this.velY -= 1;
+        this.jumping = true;
+
+      }
+    }
+
+    if (this.keys[39]) {
+      
+      if(frames % 8 === 0){
+        this.frame++
+        if(this.frame > 3) this.frame = 2
+      }
+
+        if (this.velX < this.speed) {
+            this.velX+=1.1;
+        }
+    }
+    if (this.keys[37]) {
+      if(frames % 8 === 0){
+        this.frame++
+        if(this.frame > 1) this.frame = 0
+      }
+        if (this.velX > -this.speed) {
+            this.velX-=1.1;
+        }
+    }
+
+
+}
+}
+function Player2(image){
+  this.x         = canvas.width + 30;
+  this.y         = 500;
+  this.width     = 20;
+  this.height    = 29;
+
+  this.jumping   = false;
+  this.jumpStrength = 2
+  this.velY = 0;
+  this.velX = 0;
+  this.speed = 3;
+  this.grounded = false
+  this.keys = []
+
+  this.frame     = 2;
+  this.sheet     = new Image()
+  this.sheet.src = image
+ 
+
+  this.runAnimation = function () {
+    this.x-=2
+    ctx.drawImage(this.sheet,this.frame * 21 ,0,this.width,this.height + 2, this.x, this.y,this.width,this.height)
+
+    this.y += this.velY;
+    this.velY += gravity;
+    
+    this.x += this.velX;
+    this.velX *= friction;
+  }
+  this.move = function(){
+    if(this.keys[87]){
+      if(!this.jumping){
+        this.velY = -this.jumpStrength * 2;
+        this.velY -= 1;
+        this.jumping = true;
+
+      }
+    }
+
+    if (this.keys[68]) {
+      
+      if(frames % 8 === 0){
+        this.frame++
+        if(this.frame > 3) this.frame = 2
+      }
+
+        if (this.velX < this.speed) {
+            this.velX+=1.1;
+        }
+    }
+    if (this.keys[65]) {
+      if(frames % 8 === 0){
+        this.frame++
+        if(this.frame > 1) this.frame = 0
+      }
+        if (this.velX > -this.speed) {
+            this.velX-=1.1;
+        }
+    }
+
+
+}
+}
---
+++ js/main.js
@@ -0,0 +1,334 @@
+var canvas = document.getElementById('main-game');
+var ctx = canvas.getContext('2d')
+
+var audio = ['./assets/speedotromaSong.wav']
+var images =['./assets/player/running.png','./assets/player/runningp2.png']
+
+var red = 255
+var green;
+var redDir = true;
+var color = 'rgb('+red+green+',145)'
+
+var gravity = .2
+var friction = .5;
+
+
+var background = new Background();
+var player = new Player(images[0]);
+var player2 = new Player2(images[1]);
+var blocks = [];
+var structures = [];
+var interval = 0;
+var frames = 0;
+var crazyhead = new CrazyHead()
+var mainmenuframes = 0
+
+function changeColor(){
+  if(redDir){
+    red-=.25 
+  }
+  if(!redDir){
+    red+=.25
+  }
+  if(red === 255)redDir = true
+  if(red === 0)redDir = false
+  green = red - 50
+  color = 'rgb('+red+','+green+',145)';
+}
+
+function generateBlocks(){
+  if(!(frames % 16 === 0)) return
+  var block = new Block(null,canvas.height - 32)
+  blocks.push(block)
+}
+
+function generateStructures(){
+  if(!(frames % 160 === 0)) return
+  var randomStructure = Math.floor(Math.random()*5)
+  var structure = new Structure()
+  structures.push(structure.structureSelector[3])
+}
+
+function drawBlocks(){
+  blocks.forEach(function(block){
+    block.draw()
+  })
+    structures.forEach(function(st){
+      st.forEach(function(block){
+        block.draw()
+      });
+    })
+  
+}
+
+function deleteBlocks(){
+  blocks.forEach(function(block){
+    if(block.x < -32){
+      blocks.shift(block)
+    }
+  })
+  structures.forEach(function(st){
+    st.forEach(function(block){
+        if(block.x < -300){
+          st.shift(block)
+        }
+      });
+  })
+}
+
+function collisionChecker(){
+  player.grounded = false;
+  blocks.forEach(function(block){
+    var direction = isTouching(player,block);
+    if(direction == "left" || direction == "right"){
+      player.velX = 0;
+    }else 
+    if(direction == "bottom"){
+      player.jumping = false;
+      player.grounded = true;
+    } 
+    else if(direction == "top"){
+      //player.velY *= -1
+   }
+  })
+  structures.forEach(function(st){
+    st.forEach(function(block){
+      if(block instanceof Block){
+        var direction = isTouching(player,block);
+        if(direction == "left" || direction == "right"){
+          player.velX = 0;
+        }else 
+        if(direction == "bottom"){
+          player.jumping = false;
+          player.grounded = true;
+        } 
+        else if(direction == "top"){
+          player.velY *= -1
+      }
+    }
+      if(block instanceof StickyBlock){
+        var direction = isTouching(player,block);
+        if(direction == "left" || direction == "right"){
+          player.velX = 0;
+        }else 
+        if(direction == "bottom"){
+          player.jumping = false;
+          player.grounded = true;
+        } 
+        else if(direction == "top"){
+          player.frame = 5
+          player.velY = 0
+      }
+      }
+      if(block instanceof SpikeBlock){
+        var direction = isTouching(player,block);
+        if(direction)gameOver()
+      }
+    })
+  })
+  if(player.grounded){
+    player.velY = 0;
+  }
+}
+function collisionCheckerP2(){
+  player2.grounded = false;
+  blocks.forEach(function(block){
+    var direction = isTouching(player2,block);
+    if(direction == "left" || direction == "right"){
+      player2.velX = 0;
+    }else 
+    if(direction == "bottom"){
+      player2.jumping = false;
+      player2.grounded = true;
+    } 
+    else if(direction == "top"){
+      //player.velY *= -1
+   }
+  })
+  structures.forEach(function(st){
+    st.forEach(function(block){
+      
+      if(block instanceof Block){
+        var direction = isTouching(player2,block);
+        if(direction == "left" || direction == "right"){
+          player2.velX = 0;
+        }else 
+        if(direction == "bottom"){
+          player2.jumping = false;
+          player2.grounded = true;
+        } 
+        else if(direction == "top"){
+          player2.velY *= -1
+      }
+    }
+      if(block instanceof StickyBlock){
+        var direction = isTouching(player2,block);
+        if(direction == "left" || direction == "right"){
+          player2.velX = 0;
+        }else 
+        if(direction == "bottom"){
+          player2.jumping = false;
+          player2.grounded = true;
+        } 
+        else if(direction == "top"){
+          player2.frame = 5
+          player2.velY = 0
+      }
+      }
+      if(block instanceof SpikeBlock){
+        var direction = isTouching(player2,block);
+        if(direction){
+          gameOver()
+        } 
+      }
+    })
+  })
+  if(player2.grounded){
+    player2.velY = 0;
+  }
+}
+function isTouching(char, block){
+  var vectorX = (char.x + (char.width/2)) - (block.x + (block.width/2));
+  var vectorY = (char.y + (char.height/2)) - (block.y + (block.height/2));
+  
+  var halfWidths = (char.width/2) + (block.width/2);
+  var halfHeights = (char.height/2) + (block.height/2);
+  
+  var collisionDirection = null;
+  
+  if(Math.abs(vectorX) < halfWidths && Math.abs(vectorY) < halfHeights){
+    var offsetX = halfWidths - Math.abs(vectorX);
+    var offsetY = halfHeights - Math.abs(vectorY);
+    if(offsetX < offsetY){
+      if(vectorX > 0){
+        collisionDirection = "left";
+        char.x += offsetX;
+      }else{
+        collisionDirection = "right";
+        char.x -= offsetX;
+      }
+    }else{
+      if(vectorY > 0){
+        collisionDirection = "top";
+        //char.y += offsetY;
+      }else{
+        collisionDirection = "bottom";
+        char.y -= offsetY;
+      }
+    }
+  }
+  return collisionDirection;
+  
+}
+function start(){
+document.getElementById('start').classList.add('displaynone')
+document.getElementById('start2p').classList.add('displaynone')
+clearInterval(intervalmainmenu)
+  if(interval > 0)return
+    interval = setInterval(function(){
+      update1p()
+    },1000/60)
+}
+function start2p(){
+  document.getElementById('start').classList.add('displaynone')
+  document.getElementById('start2p').classList.add('displaynone')
+  clearInterval(intervalmainmenu)
+    if(interval > 0)return
+      interval = setInterval(function(){
+        update2p()
+      },1000/60)
+  }
+function pause(){
+  clearInterval(interval)
+}
+function gameOverChecker(){
+  if(player.x + player.width < 0 || player.y - 10 > canvas.height)gameOver()
+  if(player2.x + player2.width < 0 || player2.y - 10 > canvas.height)gameOver()
+}
+function gameOver(){
+  pause()
+  document.getElementById('gameover').classList.toggle("displaynone")
+  setInterval(function(){
+    mainmenuframes++
+    finalAnimation()
+  },1000/60)
+
+}
+function finalAnimation(){
+  crazyhead.draw()
+}
+function update1p(){
+  changeColor()
+  frames++
+  ctx.clearRect(0,0,canvas.width,canvas.height)
+  background.draw();
+  generateBlocks();
+  generateStructures();
+  drawBlocks();
+  deleteBlocks();
+  collisionChecker()
+  player.runAnimation();
+  player.move()
+  gameOverChecker()
+}
+
+function update2p(){
+  changeColor()
+  frames++
+  ctx.clearRect(0,0,canvas.width,canvas.height)
+  background.draw();
+  generateBlocks();
+  generateStructures();
+  drawBlocks();
+  deleteBlocks();
+  collisionChecker()
+  collisionCheckerP2()
+  player.runAnimation();
+  player.move()
+  player2.runAnimation();
+  player2.move()
+  gameOverChecker()
+}
+
+function restart(){
+ 
+}
+
+function updateMainMenu(){
+  changeColor()
+  ctx.clearRect(0,0,canvas.width,canvas.height)
+  background.draw();
+  crazyhead.draw()
+  mainmenuframes++
+  
+}
+
+document.getElementById('start').addEventListener('click',start)
+document.getElementById('start2p').addEventListener('click',start2p)
+document.getElementById('gameover').addEventListener('click',restart)
+document.body.addEventListener("keydown", function (e) {
+  player.keys[e.keyCode] = true;
+});
+document.body.addEventListener("keyup", function (e) {
+  player.keys[e.keyCode] = false;
+});
+
+document.body.addEventListener("keydown", function (e) {
+  player2.keys[e.keyCode] = true;
+});
+document.body.addEventListener("keyup", function (e) {
+  player2.keys[e.keyCode] = false;
+});
+
+var intervalmainmenu = setInterval(function(){
+  if(interval === 0){
+    updateMainMenu()
+  }
+},1000/60)
+
+musica = new Audio()
+musica.src = audio[0]
+musica.loop = true;
+musica.play();
+
+ 
